394cdbb837dec2752dbe56ac8f9c72e3ccaf2372
EfeJemma <ogbomohjemimah@gmail.com>
2025-03-28 17:29:32 -0400

    Update gulpfile.js

--- Efes-Art-Clique/gulpfile.js
+++ Efes-Art-Clique/gulpfile.js
@@ -1,39 +1,45 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
+const {src, dest, watch, series} = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
 const browserSync = require('browser-sync').create();
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const babel = require('gulp-babel');
+const terser = require('gulp-terser');
 
 // Compile SASS and Move Font-Awesome CSS
-function scss(){
-	//1. Where is sass file
-		return gulp.src(['./node_modules/bootstrap/scss/bootstrap.scss', './assets/scss/*.scss', './node_modules/font-awesome/css/font-awesome.min.css'])
-	//2. Pass that file through sass Compiler
+function scssTask(){
+		return src('app/scss/style.scss' , { sourcemaps: true })
 		.pipe(sass())
 	//3. Where I save my CSS File
-		.pipe(gulp.dest('./assets/css/'))
-	// Browser Stream
-	.pipe(browserSync.stream())
+		.pipe(postcss([autoprefixer(), cssnano()]))
+	.pipe(dest('dist' , { sourcemaps: '.' }));
 }
 
 // Move JS file to Assets 
-function js(){
+function jsTask() {
 	//1. Where is JS file
-		return gulp.src(['./node_modules/bootstrap/dist/js/bootstrap.min.js', './node_modules/jquery/dist/jquery.min.js', './node_modules/popper.js/dist/popper.min.js'])
-	//3. Where I save my JS File 
-		.pipe(gulp.dest('./assets/js/'))
-	// Browser Stream
-	.pipe(browserSync.stream())
+		return src('app/js/script.js' , { sourcemaps: true })
+		.pipe(dest('dist', { sourcemaps: '.' }));
+	.pipe(babel({ presents: ['@babel/present-env'] }))
+	.pipe(terser())
 }
 
 // Watch SASS and Serve
-function liveServer(){
+function browserSyncServe(){
 	browserSync.init({
-		server: './'
-	});
-	gulp.watch('./assets/scss/*.scss', scss);
-	gulp.watch('./assets/js/*.js', js);
-	gulp.watch('./assets/fonts/', fonts);
-	gulp.watch('./*.html').on('change', browserSync.reload);
-}
+		server: { 
+			baseDir: '.',
+	},
+		notify; {
+		styles: {
+			top: 'auto',
+			bottom: '0',
+},
+	},
+				});
+		cb();
+	}
 
 // Move Font_Awesome Fonts Folder to Assets 
 function fonts(){
@@ -43,10 +49,18 @@ function fonts(){
 		.pipe(gulp.dest('./assets/fonts/'))
 }
 
+//Watch Task
+function watchTask() {
+	watch('*.html', browserSyncReload);
+	watch(
+		['app/scss/**/*.scss', 'app/**/*.js'],
+		series(scssTask, jsTask, browserSyncReload)
+		);
+}
+	
+//Default Gulp Task 
+	exports.default = series(scssTask, jsTask, browserSyncServe, watchTask);
 
+	//Build Gulp Task
+	exports.build = series(scssTask, jsTask);
 
-
-exports.scss = scss;
-exports.fonts = fonts;
-exports.js = js;
-exports.liveServer = liveServer;
